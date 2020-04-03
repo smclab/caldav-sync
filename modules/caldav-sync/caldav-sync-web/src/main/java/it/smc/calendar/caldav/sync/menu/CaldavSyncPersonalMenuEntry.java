4a58d8d07c9add4d719ffed1682b59bef828e2c2
Mauro Mariuzzo <mauro.mariuzzo@smc.it>
2020-04-03 18:57:42 +0200

    SF & copyright

--- modules/caldav-sync/caldav-sync-web/src/main/java/it/smc/calendar/caldav/sync/menu/CaldavSyncPersonalMenuEntry.java
+++ modules/caldav-sync/caldav-sync-web/src/main/java/it/smc/calendar/caldav/sync/menu/CaldavSyncPersonalMenuEntry.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2013 SMC Treviso Srl. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package it.smc.calendar.caldav.sync.menu;
 
 import com.liferay.product.navigation.personal.menu.BasePersonalMenuEntry;
@@ -5,17 +19,22 @@ import com.liferay.product.navigation.personal.menu.PersonalMenuEntry;
 import it.smc.calendar.caldav.sync.constants.CaldavSyncPortletKeys;
 import org.osgi.service.component.annotations.Component;
 
+/**
+ * @author rsvato
+ */
 @Component(
-        immediate = true,
-        property = {
-                "product.navigation.personal.menu.entry.order:Integer=600",
-                "product.navigation.personal.menu.group:Integer=300"
-        },
-        service = PersonalMenuEntry.class
+	immediate = true,
+	property = {
+		"product.navigation.personal.menu.entry.order:Integer=600",
+		"product.navigation.personal.menu.group:Integer=300"
+	},
+	service = PersonalMenuEntry.class
 )
 public class CaldavSyncPersonalMenuEntry extends BasePersonalMenuEntry {
-    @Override
-    protected String getPortletId() {
-        return CaldavSyncPortletKeys.CaldavSync;
-    }
+
+	@Override
+	protected String getPortletId() {
+		return CaldavSyncPortletKeys.CaldavSync;
+	}
+
 }
