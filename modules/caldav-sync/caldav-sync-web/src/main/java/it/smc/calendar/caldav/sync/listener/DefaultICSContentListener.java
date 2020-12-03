935eb01814da1f9a4d814d7974ecc8d123b425c5
Mirko Zizzari <mirko.zizzari@smc.it>
2020-12-03 16:20:01 +0100

    issue #36: extended compatibility to previous release

--- modules/caldav-sync/caldav-sync-web/src/main/java/it/smc/calendar/caldav/sync/listener/DefaultICSContentListener.java
+++ modules/caldav-sync/caldav-sync-web/src/main/java/it/smc/calendar/caldav/sync/listener/DefaultICSContentListener.java
@@ -44,6 +44,7 @@ import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.ReleaseInfo;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.kernel.util.Validator;
@@ -401,22 +402,40 @@ public class DefaultICSContentListener implements ICSImportExportListener {
 
 			TimeZone userTimeZone = user.getTimeZone();
 
-			java.util.Calendar endJCalendar = JCalendarUtil.getJCalendar(
-				calendarBooking.getEndTime(), userTimeZone);
+			TimeZone utcTimeZone = TimeZone.getTimeZone("UTC");
+
+			java.util.Calendar startJCalendar =
+				JCalendarUtil.getJCalendar(
+					calendarBooking.getStartTime(), utcTimeZone);
+
+			boolean normalize = true;
+
+			if (startJCalendar.get(java.util.Calendar.HOUR) == 0) {
+				normalize = false;
+			}
+
+			java.util.Calendar endJCalendar =
+				JCalendarUtil.getJCalendar(
+				calendarBooking.getEndTime(), utcTimeZone);
 
 			endJCalendar.add(java.util.Calendar.DAY_OF_MONTH, 1);
 
-			long utcStart = calendarBooking.getStartTime() +
-					 userTimeZone.getRawOffset();
+			long utcStart = startJCalendar.getTimeInMillis();
 
-			long utcEnd = endJCalendar.getTimeInMillis() +
-					 userTimeZone.getRawOffset();
+			long utcEnd = endJCalendar.getTimeInMillis();
+
+			if (normalize) {
+				utcStart += userTimeZone.getRawOffset();
+				utcEnd += userTimeZone.getRawOffset();
+			}
 
 			DtStart dtStart = new DtStart(
-				new net.fortuna.ical4j.model.Date(utcStart), true);
+				new net.fortuna.ical4j.model.Date(utcStart),
+				true);
 
 			DtEnd dtEnd = new DtEnd(
-				new net.fortuna.ical4j.model.Date(utcEnd), true);
+				new net.fortuna.ical4j.model.Date(utcEnd),
+				true);
 
 			propertyList.remove(vEvent.getStartDate());
 
