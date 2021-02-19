470cf3bb5ea7b440173c1101a4f9172ae10c6ab7
Mirko Zizzari <mirko.zizzari@smc.it>
2021-02-19 17:46:20 +0100

    issue #32: add portlet names upgrade process

---
+++ modules/caldav-sync/caldav-display-web/src/main/java/it/smc/calendar/caldav/display/portlet/upgrade/CalDAVDisplayWebUpgrade.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2013 SMC Treviso Srl. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package it.smc.calendar.caldav.display.portlet.upgrade;
+
+import com.liferay.portal.kernel.upgrade.DummyUpgradeStep;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+import it.smc.calendar.caldav.display.portlet.upgrade.v2_0_0.UpgradePortletId;
+
+/**
+ * @author Mirko Zizzari
+ */
+public class CalDAVDisplayWebUpgrade implements UpgradeStepRegistrator {
+
+	@Override
+	public void register(Registry registry) {
+		registry.register("0.0.0", "1.0.0", new DummyUpgradeStep());
+		registry.register("1.0.0", "2.0.0", new UpgradePortletId());
+	}
+}
