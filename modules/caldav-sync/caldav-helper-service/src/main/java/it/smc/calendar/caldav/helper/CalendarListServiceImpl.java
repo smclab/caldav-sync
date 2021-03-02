df84b78cf5f5e187853e37bc738be47d2e3713d7
Mirko Zizzari <mirko.zizzari@smc.it>
2021-03-02 21:45:53 +0100

    issue #45: avoid null and return an empty list

--- modules/caldav-sync/caldav-helper-service/src/main/java/it/smc/calendar/caldav/helper/CalendarListServiceImpl.java
+++ modules/caldav-sync/caldav-helper-service/src/main/java/it/smc/calendar/caldav/helper/CalendarListServiceImpl.java
@@ -35,6 +35,7 @@ import com.liferay.portal.kernel.util.SessionClicks;
 import com.liferay.portal.kernel.util.StringUtil;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -75,15 +76,13 @@ public class CalendarListServiceImpl implements CalendarListService {
 					List<Calendar> userCalendars = getUserCalendars(
 						permissionChecker.getUserId());
 
-					if (userCalendars != null) {
-						if (_log.isDebugEnabled()) {
-							for (Calendar calendar : userCalendars) {
-								_log.debug(" - " + calendar.getName());
-							}
+					if (_log.isDebugEnabled()) {
+						for (Calendar calendar : userCalendars) {
+							_log.debug(" - " + calendar.getName());
 						}
-
-						calendars.addAll(userCalendars);
 					}
+
+					calendars.addAll(userCalendars);
 				}
 			}
 
@@ -222,7 +221,7 @@ public class CalendarListServiceImpl implements CalendarListService {
 			_log.error(pe, pe);
 		}
 
-		return null;
+		return Collections.emptyList();
 	}
 
 	public List<Calendar> getUserGroupCalendars(
